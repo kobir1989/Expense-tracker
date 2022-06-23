781103fe7b1044d859004fd77b9f204408dd1be5
kobir hossain Ritu <kobir.h.ritu@gmail.com>
2022-06-23 22:49:54 +0600

    Display User input in Dom complited

--- UI.js
+++ UI.js
@@ -1,1 +1,36 @@
+const totalAmount = document.getElementById("total-account");
+const totalIncome = document.getElementById("total-income");
+const totalExpense = document.getElementById("total-expense");
+const historyPlus = document.getElementById("history-plus");
+const historyMinus = document.getElementById("history-minus");
+const form = document.getElementById("form");
+const amount = document.getElementById("amount");
+const text = document.getElementById("text");
+const submitBtn = document.getElementById("submit-btn");
+const containerSummary = document.querySelector(".container-summery");
 
+////////////////////////////////////////////////////////////////////////////
+
+// Display User input in UI
+const renderInputPlus = function (text, amount) {
+  const htmlPlus = `
+       <div class="row row-summery-plus">
+       <div class= "col">Date</div>
+        <div class= "col">${text}</div>
+       <div class= "col">${amount}</div>
+      </div>
+    `;
+
+  containerSummary.insertAdjacentHTML("beforeend", htmlPlus);
+};
+const renderInputMinus = function (text, amount) {
+  const htmlMinus = `
+   <div class="row row-summery-minus">
+       <div class= "col">Date</div>
+        <div class= "col">${text}</div>
+       <div class= "col">${amount}</div>
+      </div>
+    `;
+
+  containerSummary.insertAdjacentHTML("beforeend", htmlMinus);
+};
--- app.js
+++ app.js
@@ -1,1 +1,44 @@
+const dummytrans = [
+  { date: "11/02/2022", text: "Food", amount: -500 },
+  { date: "17/02/2022", text: "Salary2", amount: 2000 },
+  { date: "17/02/2022", text: "Salary", amount: 10000 },
+  { date: "21/02/2022", text: "Bills", amount: -2000 },
+];
 
+let transection = dummytrans;
+
+//calc transections
+const calcMoney = function () {
+  const transections = transection.map((item) => item.amount);
+  const totalBalance = transections
+    .reduce((accu, item) => (accu += item), 0)
+    .toFixed(2);
+
+  const income = transections
+    .filter((item) => item > 0)
+    .reduce((accu, item) => (accu += item), 0)
+    .toFixed(2);
+  const expense = transections
+    .filter((item) => item < 0)
+    .reduce((accu, item) => (accu += item), 0)
+    .toFixed(2);
+  totalAmount.textContent = `${totalBalance}`;
+  totalIncome.textContent = `${income}`;
+  totalExpense.textContent = `${expense}`;
+};
+calcMoney();
+
+// Submit BTN
+const addNewTrans = function (e) {
+  e.preventDefault();
+
+  const inputText = text.value;
+  const inputAmount = Number(amount.value);
+  if (inputAmount > 0) {
+    renderInputPlus(inputText, inputAmount);
+  } else {
+    renderInputMinus(inputText, inputAmount);
+  }
+};
+
+form.addEventListener("submit", addNewTrans);
